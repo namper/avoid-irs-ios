974fda2a9c6c635661a575a07d15926c3b939d52
Misho <nnamper@gmail.com>
2022-04-24 15:26:58 +0400

    Initial Commit

---
+++ AvoidIRS/AvoidIRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvoidIRSApp.swift
+//  AvoidIRS
+//
+//  Created by Mishiko Okropiridze on 24.04.22.
+//
+
+import SwiftUI
+
+@main
+struct AvoidIRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvoidIRS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AvoidIRS
+//
+//  Created by Mishiko Okropiridze on 24.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
